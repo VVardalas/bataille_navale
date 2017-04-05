0322ca22e57334069371a5e68bc0f20d7f07f4b6
VARDALAS VINCENT p1611561 <p1611561@univ-lyon1.fr>
2017-04-05 15:24:29 +0200

    ajustement parametres

--- src/tests/TestsBateau.java
+++ src/tests/TestsBateau.java
@@ -1,5 +1,14 @@
 package tests;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class TestsBateau {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
 
 }
